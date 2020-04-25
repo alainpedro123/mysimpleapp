984586cdcda0fb0c6c9123bbfdad42c7084bf6a8
Alain Afonso <alainpedro123@gmail.com>
2020-04-25 20:33:53 +0200

    Updates

---
+++ precache-manifest.3487405be0f37ceae1744c43c385321b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53180aaa34908387129ed5e27441ddc",
+    "url": "/mysimpleapp/index.html"
+  },
+  {
+    "revision": "a0329626b2665c62c422",
+    "url": "/mysimpleapp/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "bc8b7d7a6fb788df2c66",
+    "url": "/mysimpleapp/static/css/main.269cb67f.chunk.css"
+  },
+  {
+    "revision": "a0329626b2665c62c422",
+    "url": "/mysimpleapp/static/js/2.fb831faf.chunk.js"
+  },
+  {
+    "revision": "bc8b7d7a6fb788df2c66",
+    "url": "/mysimpleapp/static/js/main.52711329.chunk.js"
+  },
+  {
+    "revision": "f07414ea5638e0bd9da3",
+    "url": "/mysimpleapp/static/js/runtime-main.7cc5df3e.js"
+  }
+]);
